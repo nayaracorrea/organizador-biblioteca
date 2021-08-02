bdda0a11206dbfb0c19bfd25945261d1443a3f33
nayaracorrea <nayara.florentino@gmail.com>
2021-08-02 13:35:53 -0300

    refactor: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Organizador de Biblioteca")
+    .setDescription("Salve, veja, edita e exclua livros")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
